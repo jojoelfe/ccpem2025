c9d9d66424925e118d6627918dbe49ff250cbaf5
Johannes Elferich <jojotux123@hotmail.com>
2025-03-12 10:22:57 -0400

    Theme

---
+++ theme/layouts/twocols.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout default">
+    <slot />
+    <div class="flex mt-6 mb-3">
+      <div
+        class="w-1/2 mr-8"
+        :class="props.class"
+      >
+        <slot name="left" />
+      </div>
+      <div
+        class="w-1/2 ml-8"
+        :class="props.class"
+      >
+        <slot name="right" />
+      </div>
+    </div>
+    <div
+      class="bottom"
+      :class="props.class"
+    >
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
